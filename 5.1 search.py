3f8fb96ff890ce44e66aab84cc1dafbd9381d3dc
Jhy1993 <823322482@qq.com>
2016-09-05 10:47:20 +0800

    5.1 search.py

---
+++ 5.1 search.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 03 19:48:32 2016
+
+@author: Jhy1993
+"""
+v = [1, 3, 4, 2, 1, 0]
+t = 4
+def linearsearch(v, t):
+    n = len(v)
+    for i in range(n):
+        if v[i] == t:
+            return True
+    return False
+flag = linearsearch(v, t)
+print flag
+
+def sortedlinearsearch(v, t):
+    n = len(v)
+    for i in range(n):
+        if v[i] == t:
+            return True
+        elif v[i] > t:
+            return False
+    return False
+flag = sortedlinearsearch(v, t)
+print flag
+
+
+
+    pass
