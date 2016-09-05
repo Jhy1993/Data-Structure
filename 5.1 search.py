85875094386f9fdf5d8859fc07f32fefa7cd6ff5
Jhy1993 <823322482@qq.com>
2016-09-05 19:13:48 +0800

    1txt

--- 5.1 search.py
+++ 5.1 search.py
@@ -4,7 +4,7 @@ Created on Sat Sep 03 19:48:32 2016
 
 @author: Jhy1993
 """
-v = [1, 3, 4, 2, 1, 0]
+v = [1, 3, 4, 2, 1, 0, -1]
 t = 4
 def linearsearch(v, t):
     n = len(v)
@@ -12,8 +12,8 @@ def linearsearch(v, t):
         if v[i] == t:
             return True
     return False
-flag = linearsearch(v, t)
-print flag
+# flag = linearsearch(v, t)
+# print flag
 
 def sortedlinearsearch(v, t):
     n = len(v)
@@ -23,5 +23,21 @@ def sortedlinearsearch(v, t):
         elif v[i] > t:
             return False
     return False
-flag = sortedlinearsearch(v, t)
-0902-2
+# flag = sortedlinearsearch(v, t)
+
+def findSmallest(v):
+    n = len(v)
+    smallest = v[0]
+    for i in range(1, n):
+        if v[i] < smallest:
+            smallest = v[i]
+    print smallest
+    return smallest
+# findSmallest(v)
+
+def binarySearch(v, t):
+    low = 0
+    high = len(v) - 1
+    pass
+
+
